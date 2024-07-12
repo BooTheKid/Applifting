79d54c0fa1cc2157b82c885d228218449d68d33f
Alena <insolarance@seznam.cz>
2024-07-12 12:49:42 +0200

    Update tests

--- pages/buttons.js
+++ pages/buttons.js
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 export class Buttons {
     constructor(page){
         this.page = page;
-        this.services  = page.getByText('Services', { exact: true });
+        this.careers  = page.getByRole('banner').getByRole('link', { name: 'Careers' });
         this.productDiscovery = page.getByRole('link', { name: 'Product discovery', exact: true });
         this.softwareDevelopment = page.getByRole('link', { name: 'Software development' });
         this.podcast = page.getByRole('link', { name: 'Podcasts' });
@@ -11,32 +11,10 @@ export class Buttons {
     }
 
 
-    async refreshUntilServicesVisible(maxRetries = 5) {
-        let retries = 0;
-        while (retries < maxRetries) {
-          try { 
-            const isVisible = await this.services.isVisible();
-            if (isVisible) {
-              return; 
-            } else {
-              throw new Error('Services element not visible');
-            }
-          } catch (error) {
-            retries++;
-            if (retries === maxRetries) {
-              throw new Error('Services element not found');
-            }
-            await this.page.reload();
-          }
-        }
-      }
-    
       navigationByHeaderCategory = async () => {
-        await this.refreshUntilServicesVisible();
-        await this.services.hover();
-        await this.productDiscovery.waitFor({ state: 'visible' });
-        await this.productDiscovery.click();
-        await expect(this.page).toHaveURL(/.*product-discovery/);
+
+        await this.careers.click();
+        await expect(this.page).toHaveURL(/.*careers/);
       };
     
 
